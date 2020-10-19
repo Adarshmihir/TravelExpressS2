241d92ec8c937db07ba5c8d6a6b6fff7969dd2e2
Airy Goyon <airygoyon@gmail.com>
2020-10-19 19:20:06 -0400

    base project with / route to custom view

--- TP2/travelexpress/asgi.py
+++ TP2/travelexpress/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TravelExpress project.
+ASGI config for travelexpress project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TravelExpress.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelexpress.settings')
 
 application = get_asgi_application()
--- TP2/travelexpress/wsgi.py
+++ TP2/travelexpress/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TravelExpress project.
+WSGI config for travelexpress project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TravelExpress.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelexpress.settings')
 
 application = get_wsgi_application()
